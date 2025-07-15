f78029d7afc02476f5e0cf5a6bcc416d2e21badd
Zhiqiang Pan <46833647+paxaq@users.noreply.github.com>
2025-07-15 10:39:39 -0400

    Revert "Update StripeTable.tsx"
    This reverts commit 3f27f9743a6af00e01499d9df0cac91c59782dd9.

--- components/stripe/StripeTable.tsx
+++ components/stripe/StripeTable.tsx
@@ -35,19 +35,12 @@ const StripePricingTable = ({ user }: Props) => {
   return (
     <div className='flex flex-1 flex-col w-full'>
       <stripe-pricing-table
-          pricing-table-id="prctbl_1RkvllEzX6XjHR0EKfeYqznL"
-          publishable-key="pk_live_51RgzrHEzX6XjHR0E9HNs1s63LbIvXAQEyLuTpWnkmD5dR9oPALE8TSfDss0bHh6HG8IYCYBt8wkUneIUqbm8qkWO00anZ79xEg"
+          pricing-table-id="prctbl_1P0TL0C3ic5Sd20TGpWOU2Fi"
+          publishable-key="pk_live_51P0SikC3ic5Sd20T9QRaRKIkqy8l951LDgeOxcP24ZRXHnQzjnOFM7tfhsYdWksn1wNBdejJzvaxXGq0yRAxm14A00Py0XreGk"
           client-reference-id={user.id}
-          customer-email={user.email}>
+          customer-email={user.email}
+      >
       </stripe-pricing-table>
-      
-//      <stripe-pricing-table
-//          pricing-table-id="prctbl_1P0TL0C3ic5Sd20TGpWOU2Fi"
-//          publishable-key="pk_live_51P0SikC3ic5Sd20T9QRaRKIkqy8l951LDgeOxcP24ZRXHnQzjnOFM7tfhsYdWksn1wNBdejJzvaxXGq0yRAxm14A00Py0XreGk"
-//          client-reference-id={user.id}
-//          customer-email={user.email}
-//      >
-//      </stripe-pricing-table>
     </div>
   );
 }
